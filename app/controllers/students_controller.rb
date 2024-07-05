38310925a140cbe89768d72ced33c002a1b65b72
mlseligson <mseligson125@gmail.com>
2024-07-05 13:52:32 -0600

    added def summarize on line 52 of students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -49,7 +49,7 @@ class StudentsController < ApplicationController
 
     head :no_content
   end
-
+def summarize
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
